ceaf1f9bb32506c7fbd7c7859c49bc99fa9084ab
Abdelilah Ajjouqa <abdouajjouqa@gmail.com>
2025-10-12 13:22:46 +0100

    Feat: 6-Install ZOD to use it for validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,28 +1,29 @@
-export const validateRegister = (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
+// export const validateRegister = (req, res, next) => {
+//     const { firstName, lastName, email, password } = req.body;
 
-    if (!firstName || !lastName || !email || !password) {
-        return res.status(400).json({
-            message: "All field are required"
-        });
-    }
+//     if (!firstName || !lastName || !email || !password) {
+//         return res.status(400).json({
+//             message: "All field are required"
+//         });
+//     }
 
-    if (password.length < 6) {
-        return res.status(400).json({
-            message: "Password must be at least 6 characters long"
-        });
-    }
+//     if (password.length < 6) {
+//         return res.status(400).json({
+//             message: "Password must be at least 6 characters long"
+//         });
+//     }
 
-    next();
-}
+//     next();
+// }
 
-export const validateLogin = (req, res, next) => {
-    const { email, password } = req.body;
-    if(!email || !password){
-        return res.status(400).json({
-            message: "Email and password are required"
-        });
-    }
+// export const validateLogin = (req, res, next) => {
+//     const { email, password } = req.body;
+//     if(!email || !password){
+//         return res.status(400).json({
+//             message: "Email and password are required"
+//         });
+//     }
 
-    next();
-}
+//     next();
+// }
+
