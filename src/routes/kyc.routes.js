b8ca5dcf916597326eb91c91c5be0e56b85746f3
Abdelilah <abdouajjouqa@gmail.com>
2025-10-15 22:24:48 +0100

    Refactor: #57 Update KYC routes to use getUserKYC service and add public/uploads/kyc to .gitignore

--- src/routes/kyc.routes.js
+++ src/routes/kyc.routes.js
@@ -2,11 +2,11 @@ import e from 'express';
 import * as kycController from '../controllers/kyc.controller.js';
 import * as kycMiddleware from "../middlewares/auth.js";
 import upload from '../middlewares/upload.js';
-import { getKYCStatus } from '../services/kyc.service.js';
+import { getUserKYC } from '../services/kyc.service.js';
 
 const router = e.Router();
 
 router.post('/submit', kycMiddleware.authenticate, upload.single('nationalIdImage'), kycController.submitKYC);
-router.get('/status', kycMiddleware.authenticate, getKYCStatus);
+router.get('/status', kycMiddleware.authenticate, getUserKYC);
 
 export default router;
