ba800749696bb00be5c17129bbca4cc391a329ed
Abdelilah <abdouajjouqa@gmail.com>
2025-10-16 15:00:08 +0100

    Refactor: #57 Remove authentication middleware from KYC routes and improve error handling in getUserKYC controller

--- src/controllers/kyc.controller.js
+++ src/controllers/kyc.controller.js
@@ -25,3 +25,23 @@ export const submitKYC = async (req, res) => {
         });
     }
 }
+
+export const getUserKYC = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const kycVerification = await kycServices.getUserKYC(userId);
+        if (!kycVerification) {
+            return res.status(404).json({
+                message: 'No KYC found'
+            });
+        }
+        return res.status(200).json({
+            data: kycVerification
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'error',
+            error: error.message
+        })
+    }
+}
--- src/routes/kyc.routes.js
+++ src/routes/kyc.routes.js
@@ -1,12 +1,10 @@
 import e from 'express';
 import * as kycController from '../controllers/kyc.controller.js';
-import * as kycMiddleware from "../middlewares/auth.js";
 import upload from '../middlewares/upload.js';
-import { getUserKYC } from '../services/kyc.service.js';
 
 const router = e.Router();
 
-router.post('/submit', kycMiddleware.authenticate, upload.single('nationalIdImage'), kycController.submitKYC);
-router.get('/status', kycMiddleware.authenticate, getUserKYC);
+router.post('/submit', upload.single('nationalIdImage'), kycController.submitKYC);
+router.get('/status', kycController.getUserKYC);
 
 export default router;
